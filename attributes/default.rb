a8d40d3defe056c75852be0fc611d74d3e3f2ab3
Scott Richardson <dev@grogwood.com>
2020-09-26 12:09:59 -0700

    Sudo timeout attribute added to cookbook

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,11 @@
+# Default timeout (in minutes) for sudo before requesting credentials again
+default[:olyn_sudo][:config][:timeout] = 15
+
 # Allowed sudo users (built inside the wrapper recipe)
 override[:authorization][:sudo][:users] = []
 
 # Wrapper overrides
 override[:authorization][:sudo][:sudoers_defaults] = [
-  'env_reset',
+  "env_reset,timestamp_timeout=#{node[:olyn_sudo][:config][:timeout]}",
   'secure_path="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"'
 ]
